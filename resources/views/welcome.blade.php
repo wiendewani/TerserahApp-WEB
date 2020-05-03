f902bbf179612e67a082f42e9338b0127291a9d3
Hafizh <hafizhsalsabilap@gmail.com>
2020-05-03 16:33:58 +0700

    Komat Komite

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -81,7 +81,7 @@
 
             <div class="content">
                 <div class="title m-b-md">
-                    Laravel
+                    Laravel WOI TEST KOMITE
                 </div>
 
                 <div class="links">
